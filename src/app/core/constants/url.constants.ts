f7d37e5a7b7ca1208357df16a40b1f0e93d369b8
Yovani ES <yovany.21se@gmail.com>
2022-07-11 09:15:34 -0500

    CONSTANTES url

--- src/app/core/constants/url.constants.ts
+++ src/app/core/constants/url.constants.ts
@@ -1,21 +1,19 @@
-const ENVIROMENT:string = "DEV";
+const ENVIROMENT: string = 'DEV';
 
-let MAIN_PATH_NET = 'https://localhost:3061/api/configurador/'
+let MAIN_PATH_NET = 'https://localhost:3061/api/configurador/';
 
 let MAIN_PATH_AUTH = '';
-
-
 
 switch (ENVIROMENT) {
   case 'DEV':
     MAIN_PATH_AUTH = 'http://b2bsecurityservice.indratools.com/aut/seguridad';
     break;
 
-    case 'QA':
-      MAIN_PATH_AUTH = ''
+  case 'QA':
+    MAIN_PATH_AUTH = '';
     break;
 
-    case 'PROD':
+  case 'PROD':
     // MAIN_PATH = 'https://hades-back.azurewebsites.net'
     break;
 
@@ -23,11 +21,8 @@ switch (ENVIROMENT) {
     break;
 }
 
-
-export const API_AUTH_SESSION = MAIN_PATH_AUTH + '/login'
+export const API_AUTH_SESSION = MAIN_PATH_AUTH + '/login';
 
 // REGISTRO
-export const API_REG1 = MAIN_PATH_NET + 'ExecuteQuery'
-export const API_REG2 = MAIN_PATH_NET + 'getcustomquery/' +'5-xxx-0000';
-
-
+export const API_REG1 = MAIN_PATH_NET + 'ExecuteQuery';
+export const API_REG2 = MAIN_PATH_NET + 'getcustomquery/' + '5-xxx-0000';
